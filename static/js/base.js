d690212c7ad9ad6e0ae03a80b459cbe0288cce17
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-07-17 00:20:42 +0900

    -

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,41 @@
 var app = new Vue({
   el: '#app',
   data: {
+    user: null,
     message: 'Hello Vue!'
+  },
+  methods: {
+    login: function () {
+      console.log("doSomething");
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
   }
-})
+});
+
+var user = firebase.auth().currentUser;
+console.log("User=" + user)
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("Logined: " + user.email)
+    app.user = user.email
+    // User is signed in.
+  } else {
+    console.log("No Login:")
+    app.user = null
+    // No user is signed in.
+  }
+});
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+  }
+  var user = result.user;
+  console.log(result);
+  console.log("getRedirect Success " + user)
+}).catch(function(error) {
+  console.log("getRedirect Fail " + error.message)
+});
---
+++ static/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBECvMxLcjQh7VxT8XWgwGCceIZasEYX2c",
+  authDomain: "isucon-bench.firebaseapp.com",
+  databaseURL: "https://isucon-bench.firebaseio.com",
+  projectId: "isucon-bench",
+  storageBucket: "isucon-bench.appspot.com",
+  messagingSenderId: "1045922409294"
+};
+firebase.initializeApp(config);
